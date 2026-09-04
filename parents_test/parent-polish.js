4deb0a245bcc8a517c48cbd86d84ff4ab1161a03
medsi-children <ori.space.cat@gmail.com>
2026-09-04 16:49:46 +0300

    Point parent chat button to standalone chat renderer

--- parents_test/parent-polish.js
+++ parents_test/parent-polish.js
@@ -55,7 +55,7 @@
 
   function installChatRoute(){
     const btn=$('btnChat');
-    if(btn)btn.onclick=()=>{location.href='/parents_test/chat.html?v=radical1'};
+    if(btn)btn.onclick=()=>{location.href='/parents_test/chat.html?v=standalone2'};
   }
 
   function install(){
